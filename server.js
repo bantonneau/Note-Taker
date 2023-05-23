0220471ac75e48c0774dacea84bad84983d6fcbb
benos <benantonn@gmail.com>
2023-05-23 12:41:09 -0500

    Set up server.js, middleware, html routes

--- server.js
+++ server.js
@@ -9,3 +9,14 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html")
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
