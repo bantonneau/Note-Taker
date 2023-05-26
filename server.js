66b1308a331d388979e014489ec25bd488d8e64a
benos <benantonn@gmail.com>
2023-05-25 20:35:38 -0500

    add db read/write functions + post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const notesData = require("./db/db.json");
+const fs = require("fs");
 
 const PORT = 3001;
 const app = express();
@@ -8,6 +8,20 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const dbPath = path.join(__dirname, "db", "db.json");
+
+
+// Function to read the notes from the db.json file
+function readNotes() {
+    const data = fs.readFileSync(dbPath, "utf8");
+    return JSON.parse(data);
+}
+
+// Function to write the notes to the db.json file
+function writeNotes(notes) {
+    fs.writeFileSync(dbPath, JSON.stringify(notes, null, 2));
+}
 
 app.get("*", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
@@ -17,6 +31,15 @@ app.get("/notes", (req, res) => {
     res.sendFile(__dirname + "/public/notes.html")
 });
 
+app.post("/api/notes", (req, res) => {
+    const notes = readNotes();
+    const newNote = req.body;
+    newNote.id = Date.now().toString();
+    notes.push(newNote);
+    writeNotes(notes);
+    res.json(newNote);
+  });
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
